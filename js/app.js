aee937fbebba04a459a7db1a030ced43a28cde81
William Rojas <william.ur@live.com>
2020-12-29 20:06:59 -0700

    added smooth scrool when clicking on a tag

--- js/app.js
+++ js/app.js
@@ -19,7 +19,46 @@ function appendChildren (parent, children) {
 const myList = document.getElementById('navbar__list');
 
 //add text to lis and also a tag to make links
-let items = [createList('<a href="#">Section 1</a>'), 
-createList('<a href="#">Section 2</a>'), createList('<a href="#">Section 3</a>')];
+let items = [createList('<a id="a" href="#">Section 1</a>'), 
+createList('<a id="b" href="#">Section 2</a>'), createList('<a id="c" href="#">Section 3</a>')];
 
 appendChildren(myList, items);
+
+
+//WHen clicking on link in Nav bar, will take you to specific section with smooth transition
+
+const sectionOne = document.getElementById('a');
+const sectionTwo = document.getElementById('b');
+const sectionThree = document.getElementById('c');
+
+  sectionOne.addEventListener('click', {
+    handleEvent: function (event) {
+        window.scrollTo({
+            top: 350,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+  });
+
+  sectionTwo.addEventListener('click', {
+    handleEvent: function (event) {
+        const sectionDos = document.querySelector('#section2');
+        sectionDos.classList.toggle('sectionDos');
+        window.scrollTo({
+            top: 1200,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+  });
+
+  sectionThree.addEventListener('click', {
+    handleEvent: function (event) {
+        window.scrollTo({
+            top: 1900,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+  });
